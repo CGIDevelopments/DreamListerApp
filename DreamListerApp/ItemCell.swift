0db39623f457386b278373db4c90b93da643d4ce
Codie Featherstone <cgi-lifebook@outlook.com>
2016-09-03 14:51:30 +0100

    Init 4
    Init at video 66 Part 3-Connecting the views

---
+++ DreamListerApp/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamListerApp
+//
+//  Created by Codie Featherstone on 03/09/2016.
+//  Copyright © 2016 CgiDevelopments. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+ 
+
+}
