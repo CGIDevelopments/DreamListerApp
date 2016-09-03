67d945e9d2743412faa4c07951ad12532a2a5bb3
Codie Featherstone <cgi-lifebook@outlook.com>
2016-09-03 14:32:21 +0100

    Init 3
    Init at video 65

--- DreamListerApp/MainVC.swift
+++ DreamListerApp/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamListerApp
 //
 //  Created by Codie Featherstone on 02/09/2016.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- ItemType+CoreDataClass.swift
+++ ItemType+CoreDataClass.swift
@@ -11,5 +11,5 @@ import CoreData
 
 
 public class ItemType: NSManagedObject {
-
-}
+    
+    }
