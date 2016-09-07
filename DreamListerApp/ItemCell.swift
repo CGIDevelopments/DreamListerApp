d548ed36e793360501ead7856a1d75040b4a3ec2
Codie Featherstone <cgi-lifebook@outlook.com>
2016-09-07 03:07:19 +0100

    Init 6
    Init after video 68

--- DreamListerApp/ItemCell.swift
+++ DreamListerApp/ItemCell.swift
@@ -14,6 +14,13 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "£\(item.price)"
+        details.text = item.details
+    }
  
 
 }
--- DreamListerApp/MainVC.swift
+++ DreamListerApp/MainVC.swift
@@ -24,27 +24,54 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         tableView.delegate = self
         tableView.dataSource = self
         
+        //generateTestData()
+        attemptFetch()
+        
     }
     
     
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        
+        return cell
     }
     
+    
+    func configureCell(cell: ItemCell, indexPath: NSIndexPath) {
+        
+        let item = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: item)
+        
+    }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        if let sections = controller.sections {
+        
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
     }
-    
+    return 0
+        
+    }
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
         
+        if let sections = controller.sections {
+            return sections.count
+        }
         return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
     
@@ -56,6 +83,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
+        self.controller = controller
+        
         do {
             
             try controller.performFetch()
@@ -63,6 +92,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             
             let error = error as NSError
             print("\(error)")
+            
+            
             
         }
         
@@ -100,8 +131,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             break
         case.update:
             if let indexPath = indexPath {
-                tableView.deleteRows(at: [indexPath], with: .fade) as! ItemCell
-                // update the cell data
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
             }
             break
         case.move:
@@ -116,6 +147,30 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         }
         
     }
+    
+    
+    func generateTestData() {
+        
+        let item = Item(context: context)
+        item.title = "MacBook Pro"
+        item.price = 1800
+        item.details = "I can't wait until the September event, I hope they release new MacBook Pro's."
+        
+        let item2 = Item(context: context)
+        item2.title = "Bose Headphones"
+        item2.price = 300
+        item2.details = "But man, its so nice to block out everyone with the noise canceling tech."
+        
+        let item3 = Item(context: context)
+        item3.title = "Tesla Model S"
+        item3.price = 110000
+        item3.details = "Oh man this is a beautiful car. One day, i will own one."
+        
+        ad.saveContext()
+    }
+    
+    
+    
 
     
     
